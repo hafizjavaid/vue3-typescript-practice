cc824d3b2fa43fe9cd0aa12d6ff9f02cb02967b3
hafizjavaid <hafizjaidi786@gmail.com>
2021-12-30 18:23:26 +0500

    Finshing

--- src/components/Jobs.vue
+++ src/components/Jobs.vue
@@ -4,7 +4,7 @@
       {{
         order
       }}
-      <li v-for="job in jobs" :key="job.id">
+      <li v-for="job in orderedJobs" :key="job.id">
         <h2>{{ job.title }} in {{ job.location }}</h2>
         <div class="salary">
           <p>{{ job.salary }} rupees</p>
@@ -23,7 +23,7 @@
 </template>
 
 <script lang="ts">
-import { defineComponent, PropType } from "vue";
+import { computed, defineComponent, PropType } from "vue";
 import Job from "@/types/Job";
 import OrderTerm from "@/types/OrderTerm";
 
@@ -40,6 +40,14 @@ export default defineComponent({
   },
   setup(props) {
     console.log(props.jobs);
+    const orderedJobs = computed(() => {
+      return [...props.jobs].sort((a: Job, b: Job) => {
+        return a[props.order] > b[props.order] ? 1 : -1;
+      });
+    });
+    return {
+      orderedJobs,
+    };
   },
 });
 </script>
