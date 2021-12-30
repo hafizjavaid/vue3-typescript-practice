eaadb1a0d202fb3c71bd4aecf4606a12226c77d7
hafizjavaid <hafizjaidi786@gmail.com>
2021-12-30 17:11:05 +0500

    Done with lesson 1

---
+++ src/App.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="app">
+    <p>{{ name }} - {{ age }}</p>
+    <button @click="changeAge(30)">changeAge</button>
+    <button @click="changeName('Javaid')">changeName</button>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+
+export default defineComponent({
+  name: "App",
+  components: {},
+  data() {
+    return {
+      name: "Hafiz" as string,
+      age: 25 as number | string,
+    };
+  },
+  methods: {
+    changeAge(age: number | string) {
+      this.age = age;
+    },
+    changeName(name: string) {
+      this.name = name;
+    },
+  },
+});
+</script>
+
+<style></style>
